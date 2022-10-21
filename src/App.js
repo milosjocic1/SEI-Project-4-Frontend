c803f9fa398af64d5387b71a654d58b79e4d1d14
Claudia Boden <clauboden@hotmail.com>
2022-10-21 12:59:18 +0100

    Started BS nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+// Components
+import Home from "./Home";
+
+// Bootstrap
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+// Router
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+// Css
+import "./App.css";
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Navbar expand="lg">
+          <Container>
+            <Navbar.Brand>
+              {" "}
+              <Link to="/">
+                <img className='logo' src="AGORA-LOGO.png"></img>
+              </Link>{" "}
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link to="/"> Empy link now </Link>&nbsp;
+                <Link to="/"> Empy link now </Link>&nbsp;
+                <Link to="/"> Empy link now </Link>&nbsp;
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<Home />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
