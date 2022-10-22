5c75e240dff80b0772cfd7fedffaf42684c4927c
Milos Jocic <milosjocicpt@gmail.com>
2022-10-22 23:14:07 +0100

    cards aligned, yet to figure out maintaining equal card size with different images

--- src/product/ProductList.js
+++ src/product/ProductList.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import '../ProductList.css'
 
 export default function ProductList() {
   return (
-    <div>
-    <Card style={{width: '18rem'}}>
-        <Card.Img variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
+    <div className="product-card-index">
+    <Card style={{width: '18rem'}} className="product-card">
+        <Card.Img className="card-img" variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
         <Card.Body>
             <Card.Title>White T-Shirt</Card.Title>
             <Card.Text>
@@ -16,8 +17,18 @@ export default function ProductList() {
             <Button variant="primary">Add to Faves</Button>
         </Card.Body>
     </Card>
-    <Card style={{width: '18rem'}}>
-        <Card.Img variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
+    <Card style={{width: '18rem'}} className="product-card">
+        <Card.Img className="card-img" variant="top" src="https://cdn.shopify.com/s/files/1/0419/1525/files/2800x1080-Moto-Black-020122-1_800x533_crop_right.jpg?v=1643761804"/>
+        <Card.Body>
+            <Card.Title>Leather Jacket</Card.Title>
+            <Card.Text>
+                Muito bommmm
+            </Card.Text>
+            <Button variant="primary">Add to Faves</Button>
+        </Card.Body>
+    </Card>
+    <Card style={{width: '18rem'}} className="product-card">
+        <Card.Img className="card-img" variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
         <Card.Body>
             <Card.Title>White T-Shirt</Card.Title>
             <Card.Text>
@@ -26,8 +37,8 @@ export default function ProductList() {
             <Button variant="primary">Add to Faves</Button>
         </Card.Body>
     </Card>
-    <Card style={{width: '18rem'}}>
-        <Card.Img variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
+    <Card style={{width: '18rem'}} className="product-card">
+        <Card.Img className="card-img" variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
         <Card.Body>
             <Card.Title>White T-Shirt</Card.Title>
             <Card.Text>
@@ -36,8 +47,8 @@ export default function ProductList() {
             <Button variant="primary">Add to Faves</Button>
         </Card.Body>
     </Card>
-    <Card style={{width: '18rem'}}>
-        <Card.Img variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
+    <Card style={{width: '18rem'}} className="product-card">
+        <Card.Img className="card-img" variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
         <Card.Body>
             <Card.Title>White T-Shirt</Card.Title>
             <Card.Text>
@@ -46,8 +57,8 @@ export default function ProductList() {
             <Button variant="primary">Add to Faves</Button>
         </Card.Body>
     </Card>
-    <Card style={{width: '18rem'}}>
-        <Card.Img variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
+    <Card style={{width: '18rem'}} className="product-card">
+        <Card.Img className="card-img" variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
         <Card.Body>
             <Card.Title>White T-Shirt</Card.Title>
             <Card.Text>
@@ -56,8 +67,8 @@ export default function ProductList() {
             <Button variant="primary">Add to Faves</Button>
         </Card.Body>
     </Card>
-    <Card style={{width: '18rem'}}>
-        <Card.Img variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
+    <Card style={{width: '18rem'}} className="product-card">
+        <Card.Img className="card-img" variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
         <Card.Body>
             <Card.Title>White T-Shirt</Card.Title>
             <Card.Text>
@@ -66,18 +77,8 @@ export default function ProductList() {
             <Button variant="primary">Add to Faves</Button>
         </Card.Body>
     </Card>
-    <Card style={{width: '18rem'}}>
-        <Card.Img variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
-        <Card.Body>
-            <Card.Title>White T-Shirt</Card.Title>
-            <Card.Text>
-                True to size white t-shirt
-            </Card.Text>
-            <Button variant="primary">Add to Faves</Button>
-        </Card.Body>
-    </Card>
-    <Card style={{width: '18rem'}}>
-        <Card.Img variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
+    <Card style={{width: '18rem'}} className="product-card">
+        <Card.Img className="card-img" variant="top" src="https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/449717/item/goods_00_449717.jpg?width=480&impolicy=quality_70&imformat=chrome"/>
         <Card.Body>
             <Card.Title>White T-Shirt</Card.Title>
             <Card.Text>
