e4a421cc985d65a7429abb3123a8a6baa3c162f0
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-26 10:36:49 +0100

    Fixing merge issues

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import "./Auth.css"
 
 
-import { BrowserRouter as Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 
