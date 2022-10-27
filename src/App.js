0bf10141f2d9aef7d960a61e8eda65b0270d35c1
alex-sasha-ward <amwtranslations@gmail.com>
2022-10-27 22:30:09 +0100

    Updates to style and cart counter

--- src/App.js
+++ src/App.js
@@ -30,6 +30,8 @@ export default function App() {
   const [user, setUser] = useState({});
   const [message, setMessage] = useState(null);
 
+  const [counter, setCounter] = useState(0);
+
   useEffect(() => {
     let token = localStorage.getItem("token");
     if (token != null) {
@@ -67,6 +69,20 @@ export default function App() {
     loadProductList();
   };
 
+  const buyItem = (id, productId) => {
+    Axios.post(`/cart?userId=${id}&productId=${productId}`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
+
+  const counterUp = () => {
+    setCounter(prevCount => prevCount + 1);
+  }
   //  const errMessage = message ? (
   //    <Alert variant="danger"> {message}</Alert>
   //  ) : null;
@@ -220,13 +236,12 @@ export default function App() {
             <Nav className="">
               {isAuth ? (
                 <div>
-                  <Link to="/"> HOME </Link>
+                  <Link to="/">HOME</Link>&nbsp;
                   {user ? "Welcome " + user.name : null}&nbsp;
-                  <Link to="/user/dashboard"> MY ACCOUNT </Link>
-                  <Link to="/productlist"> Product List </Link>
-                  &nbsp;&nbsp;&nbsp;
-                  <Link to="/product"> Single Product </Link>
-                  <Link to="/cart">Cart</Link>
+                  <Link to="/user/dashboard">MY ACCOUNT</Link>&nbsp;
+                  <Link to="/productlist">Product List</Link>&nbsp;
+                  <Link to="/product">Single Product</Link>&nbsp;
+                  <Link to="/cart">Cart {counter}</Link>&nbsp;
                   <Link to="/logout" onClick={onLogoutHandler}>
                     Logout
                   </Link>{" "}
@@ -276,7 +291,7 @@ export default function App() {
           ></Route>
           <Route
             path="/productlist/*"
-            element={<ProductList user={user} products={products} />}
+            element={<ProductList user={user} products={products} buyItem={buyItem}/>}
           ></Route>
           <Route
             path="/product/:productId/*"
@@ -285,6 +300,8 @@ export default function App() {
                 product={products}
                 category={allCategories}
                 user={user}
+                buyItem={buyItem}
+                counterUp={counterUp}
               />
             }
           ></Route>
