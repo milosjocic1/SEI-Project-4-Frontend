885e713a4ccb19344b2258532b0b31ee31a5cb5e
ellieeadington <63414517+ellieeadington@users.noreply.github.com>
2022-10-27 19:40:32 +0100

    shipping & billing api and forms + price totals for cart

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,7 +1,73 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import CartItem from "../cart/CartItem";
+import { Form, Button } from "react-bootstrap";
+
     export default function Cart(props){
+
+      const [newShipping, setNewShipping] = useState(props.user.shippingAddress);
+      const [newBilling, setNewBilling] = useState(props.user.billingAddress);
+
+      const editShipping = (newShipping) => {
+        Axios.put('/shipping_billing/update', newShipping, {
+            headers: {
+                "Authorisation":"Bearer " + localStorage.getItem("token")
+            }   
+        })
+        .then(response => {
+            console.log('Shipping details updated successfully');
+            console.log(response)
+            // loadAuthorList();
+        })
+        .catch(error => {
+            console.log('Error editing shipping details');
+            console.log(error)
+        })
+   }
+
+    const editBilling = (newBilling) => {
+      Axios.put('/shipping_billing/update', newBilling, {
+          headers: {
+              "Authorisation":"Bearer " + localStorage.getItem("token")
+          }   
+      })
+      .then(response => {
+          console.log('Billing details updated successfully');
+          console.log(response)
+          // loadAuthorList();
+      })
+      .catch(error => {
+          console.log('Error editing billing details');
+          console.log(error)
+      })
+}
+      
+ 
+      
+      const handleChangeS = (event) => {
+        const attributeToChange = event.target.name;
+        const newValue = event.target.value;
+        const shipping = { ...newShipping };
+        shipping[attributeToChange] = newValue;
+        setNewShipping(shipping);
+      };
+
+      const handleChangeB = (event) => {
+        const attributeToChange = event.target.name;
+        const newValue = event.target.value;
+        const billing = { ...newBilling};
+        billing[attributeToChange] = newValue;
+        setNewBilling(billing);
+      };
+
+    
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        editShipping(newShipping);
+        editBilling(newBilling);
+        event.target.reset();
+      }
+
       const [cart, setCart] = useState([]);
       useEffect(() => {
         loadCartList();
@@ -48,7 +114,108 @@ import CartItem from "../cart/CartItem";
             <br/>
             <div>Cart Total: £{cart.total}</div>
             <br/>
-            <button>Continue to Checkout</button>
-          </div>
+            <Button  variant="primary" value="Update Product" >Continue to  Shipping</Button >
+
+          <div className="container">
+          <h3>Shipping Address</h3>
+          <Form onSubmit={handleSubmit}>
+
+            <Form.Group>  
+              <Form.Label>Address Line 1</Form.Label>
+              <Form.Control
+                name="addressLine1S"
+                type="text"
+                onChange={handleChangeS}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Address Line 2</Form.Label>
+              <Form.Control
+                name="addressLine2S"
+                type="text"
+                onChange={handleChangeS}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                name="cityS"
+                type="text"
+                onChange={handleChangeS}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>County</Form.Label>
+              <Form.Control
+                name="countyS"
+                type="text"
+                onChange={handleChangeS}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>PostCode</Form.Label>
+              <Form.Control
+                name="priceS"
+                type="text"
+                onChange={handleChangeS}
+              ></Form.Control>
+            </Form.Group>
+            <h3>Billing Address</h3>
+
+            <Form.Group>  
+              <Form.Label>Address Line 1</Form.Label>
+              <Form.Control
+                name="addressLine1B"
+                type="text"
+                onChange={handleChangeB}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Address Line 2</Form.Label>
+              <Form.Control
+                name="addressLine2B"
+                type="text"
+                onChange={handleChangeB}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                name="cityB"
+                type="text"
+                onChange={handleChangeB}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>County</Form.Label>
+              <Form.Control
+                name="countyB"
+                type="text"
+                onChange={handleChangeB}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>PostCode</Form.Label>
+              <Form.Control
+                name="postCodeB"
+                type="text"
+                onChange={handleChangeB}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button variant="primary" value="Update Product" type="submit">
+              Checkout
+            </Button>
+          </Form>
+        </div>
+        </div>
         );
       }
