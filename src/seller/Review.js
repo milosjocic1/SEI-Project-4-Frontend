0c6ef6a98e3e62f823aa4f604bc7c48e69fc9dea
Claudia Boden <clauboden@hotmail.com>
2022-10-28 10:04:40 +0100

    Added no reviews yet

--- src/seller/Review.js
+++ src/seller/Review.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Review() {
   return (
     <div className='mt-5 mb-5'>
-        <h2>Reviews Component</h2>
+        <h2>You have no reviews yet!</h2>
         
     </div>
   )
