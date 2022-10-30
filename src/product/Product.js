ae46e1b1fb897ad4052fcb447f0f9b0cae2b5b13
Claudia Boden <clauboden@hotmail.com>
2022-10-30 13:43:24 +0000

    Added Modal popup to redirect to cart after clicking on Buy

--- src/product/Product.js
+++ src/product/Product.js
@@ -3,6 +3,8 @@ import Categories from "../components/Categories";
 import Search from "../components/Search";
 import Review from "../seller/Review";
 import { useParams } from "react-router-dom";
+import Modal from "react-bootstrap/Modal";
+import { Link } from "react-router-dom"
 
 
 export default function Product(props) {
@@ -10,15 +12,33 @@ export default function Product(props) {
   const thisProduct = props.product.find(
     (product) => product._id === productId
   );
-  console.log(props)
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [title, setTitle] = React.useState("Transitioning...");
+
+  const showModal = () => {
+    setIsOpen(true);
+  };
+
+  const hideModal = () => {
+    setIsOpen(false);
+    setTitle("Transitioning...");
+  };
+
+  const modalLoaded = () => {
+    setTitle("Product added to your cart!");
+  };
+
+
+  
 
   return (
     <div className="container">
-      
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <Search></Search>
-      <div className="categories-single-product">
-        <Categories category={props.category}></Categories>
-      </div>
 
       {/* PRODUCT DETAILS */}
 
@@ -34,19 +54,32 @@ export default function Product(props) {
         <div className="col-md-6">
           <h1>{thisProduct.title}</h1>
           <h4> {thisProduct.subTitle} </h4> <br />
-          <br /> <br /> <br />
+          <br />
           <p>
             Condition:{" "}
             <span>
               <strong>{thisProduct.condition}</strong>
             </span>
           </p>
-          <p>
-            Sold by:{" "}
-            <span>
-              <strong>{thisProduct.seller[0].sellerName}</strong>
-            </span>
-          </p>
+          <div className="row">
+            <div className="col-4">
+              <p>
+                Sold by:{" "}
+                <span>
+                  <strong>{thisProduct.seller[0].sellerName}</strong>
+                </span>
+              </p>
+            </div>
+            <div className="col-8">
+              <p>
+                Rating:{" "}
+                <span>
+                  <strong>{thisProduct.seller[0].sellerName}</strong>
+                </span>
+              </p>
+            </div>
+          </div>
+          <br />
           <p>
             Shipping fee to the UK:{" "}
             <span>
@@ -55,9 +88,40 @@ export default function Product(props) {
             </span>
           </p>{" "}
           <br />
+          <h2>
+            <span>Price: </span>£{thisProduct.price}
+          </h2>{" "}
           <br />
-          <h2>£{thisProduct.price}</h2> <br />
-          <button className="buy-btn" onClick={() => {props.buyItem(props.user.id, thisProduct._id); props.counterUp()}}>Buy</button>
+          <button
+            className="buy-btn"
+            onClick={() => {
+              props.buyItem(props.user.id, thisProduct._id);
+              props.counterUp();
+              showModal();
+            }}
+          >
+            Buy
+          </button>
+          {/* MODAL */}
+          <Modal
+            as="section"
+            show={isOpen}
+            onHide={hideModal}
+            onEntered={modalLoaded}
+          >
+            <Modal.Header>
+              <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>What would you like to do next?</Modal.Body>
+            <Modal.Footer>
+              <button className="buy-btn" onClick={hideModal}>
+                Continue Shopping
+              </button>
+              <Link to="/cart">
+                <button className="buy-btn">Go to Cart</button>
+              </Link>
+            </Modal.Footer>
+          </Modal>
         </div>
       </div>
       <div className="mt-5 mb-5">
@@ -70,6 +134,10 @@ export default function Product(props) {
       <div>
         <h5>Recently viewed cards - possibly</h5>
       </div>
+      <div className="categories-single-product">
+        <Categories category={props.category}></Categories>
+      </div>
+      {/* MODAL - CONTINUE SHOPPING OR CART */}
     </div>
   );
 }
