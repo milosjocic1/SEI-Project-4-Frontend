a19cafc1f6732cba89a6c462a4538bb08c672504
Claudia Boden <clauboden@hotmail.com>
2022-11-01 18:18:00 +0000

    poull

--- src/seller/MyProducts.js
+++ src/seller/MyProducts.js
@@ -1,73 +1,132 @@
-import React, { useState } from "react";
-import ProductCreateForm from "../product/ProductCreateForm";
-// import ProductEditForm from "../product/ProductEditForm";
-import "../App.css";
-import MyProductList from "./MyProductList";
-// import axios from "axios";
-
-export default function MyProducts(props) {
-  const [showAddProductForm, setShowAddProductForm] = useState(false);
-
-  // const [showAllItems, setShowAllItems] = useState(allProducts)
-  
-
-  // const all
-
-  //   SHOW SELLER RPDOCUTS ONLY
-
-  const myProducts = props.products
-    .filter((product) => {
-      if (product.seller.length) {
-        const { seller } = product;
-        const [sellerInfo] = seller;
-        const productSellerId = sellerInfo._id;
-
-        return productSellerId === props.seller._id;
-      }
-    })
-    .map((product) => {
-      return <MyProductList key={product._id} {...product} loadProductList={props.loadProductList} />;
-    });
-
-  const handleShowAddProductForm = (boolean) => {
-    setShowAddProductForm(boolean);
+import React from "react";
+import Categories from "../components/Categories";
+import Search from "../components/Search";
+import Review from "../seller/Review";
+import { useParams } from "react-router-dom";
+import Modal from "react-bootstrap/Modal";
+import { Link } from "react-router-dom";
+export default function Product(props) {
+  const { productId } = useParams();
+  const thisProduct = props.product.find(
+    (product) => product._id === productId
+  );
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [title, setTitle] = React.useState("Transitioning...");
+  const showModal = () => {
+    setIsOpen(true);
   };
-  
+  const hideModal = () => {
+    setIsOpen(false);
+    setTitle("Transitioning...");
+  };
+  const modalLoaded = () => {
+    setTitle("Product added to your cart!");
+  };
   return (
-    <div>
-      <button
-        className="index-price-button add-product"
-        onClick={() => {
-          setShowAddProductForm(true);
-        }}
-      >
-        Add a Product!
-      </button>
-
-      {showAddProductForm ? (
-        <div>
-          <ProductCreateForm
-            functions={props}
-            seller={props.seller}
-            user={props.user}
-            handleProductFileInputChange={props.handleProductFileInputChange}
-            previewSourceProduct={props.previewSourceProduct}
-            previewProductFile={props.previewProductFile}
-            handleSubmitFileProduct={props.handleSubmitFileProduct}
-            loadProductList={props.loadProductList}
+    <div className="container">
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Search></Search>
+      {/* PRODUCT DETAILS */}
+      <div className="row d-flex mt-6 product-details-div">
+        <p className="subcategories">{thisProduct.subCategory}</p>
+        <div className="col-md-6">
+          <img
+            src={`${thisProduct.cloudinary_url}`}
+            alt={`${thisProduct.cloudinary_url}`}
+            className="product-image-detail"
           />
         </div>
-      ) : (
-        <div> </div>
-      )}
-      <div>
-        <div className="row card-group mt-1 mb-3">
-          <h3>My Listings</h3>
-          <br></br>
-          <br></br>
-          {!!myProducts.length ? myProducts : <p>No listings yet!</p>}
+        <div className="col-md-6">
+          <h1>{thisProduct.title}</h1>
+          <h4> {thisProduct.subTitle} </h4> <br />
+          <br />
+          <p>
+            Condition:{" "}
+            <span>
+              <strong>{thisProduct.condition}</strong>
+            </span>
+          </p>
+          <div className="row">
+            <div className="col-4">
+              <p>
+                Sold by:{" "}
+                <span>
+                  <strong>{thisProduct.seller[0].sellerName}</strong>
+                </span>
+              </p>
+            </div>
+            <div className="col-8">
+              <p>
+                Rating:{" "}
+                <span>
+                  <strong>{thisProduct.seller[0].sellerName}</strong>
+                </span>
+              </p>
+            </div>
+          </div>
+          <br />
+          <p>
+            Shipping fee to the UK:{" "}
+            <span>
+              {" "}
+              <strong>£{thisProduct.shippingRate}</strong>{" "}
+            </span>
+          </p>{" "}
+          <br />
+          <h2>
+            <span>Price: </span>£{thisProduct.price}
+          </h2>{" "}
+          <br />
+          <button
+            className="buy-btn"
+            onClick={() => {
+              props.buyItem(props.user.id, thisProduct._id);
+              props.counterUp();
+              showModal();
+            }}
+          >
+            Buy
+          </button>
+          {/* MODAL */}
+          <Modal
+            as="section"
+            show={isOpen}
+            onHide={hideModal}
+            onEntered={modalLoaded}
+          >
+            <Modal.Header>
+              <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>What would you like to do next?</Modal.Body>
+            <Modal.Footer>
+              <button className="buy-btn" onClick={hideModal}>
+                Continue Shopping
+              </button>
+              <Link to="/cart">
+                <button className="buy-btn">Go to Cart</button>
+              </Link>
+            </Modal.Footer>
+          </Modal>
         </div>
       </div>
+      <div className="mt-5 mb-5">
+        <h2>Product Description</h2>
+        <p className="product-description">{thisProduct.description}</p> <br />
+        <h2>Returns Policy</h2>
+        <p>{thisProduct.returnsPolicy}</p>
+      </div>
+      <Review></Review>
+      <div>
+        <h5>Recently viewed cards - possibly</h5>
+      </div>
+      <div className="categories-single-product">
+        <Categories category={props.category}></Categories>
+      </div>
+      {/* MODAL - CONTINUE SHOPPING OR CART */}
     </div>
   );
 }
--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -2,25 +2,20 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import BuyerAccount from "./BuyerAccount";
 import SellerAccount from "../seller/SellerAccount";
-
 import "./User.css";
-
 export default function UserDashboard(props) {
   // const [products, setProducts] = useState([]);
   const [currentUser, setCurrentUser] = useState([]);
   const [seller, setSeller] = useState({});
   const [isEdit, setIsEdit] = useState(false);
   const [currentProduct, setCurrentProduct] = useState({});
-
   // useEffect() - ComponentDidMount
   // React Hooks - Allows to hook the functionality into React
-
   useEffect(() => {
     // Axios function
     loadDashboard(props.user.id);
   }, []);
-  console.log(props)
-
+  console.log(props);
   const loadDashboard = (id) => {
     Axios.get(`/user/dashboard/?userId=${id}`)
       .then(({ data }) => {
@@ -31,7 +26,7 @@ export default function UserDashboard(props) {
         console.log(error);
       });
   };
-console.log(props.user)
+  console.log(props.user);
   return (
     <div>
       {props.user.userRole === "buyer" ? (
